2bd5138660cea2d20e6f1ecd11705d9771418847
Oleg Lazeba <lazebaoleg@gmail.com>
2023-03-04 22:06:37 +0200

    lesson_2

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+# https://www.youtube.com/watch?v=cBt7DveuuS0&list=PLB2iiSfKWtvykq9s0plSVI_Du60i0iphU&index=3
---
+++ tests/something_test.py
@@ -0,0 +1,8 @@
+
+
+def test_equal():
+    assert 1 == 1, "number is not equal expected"
+
+
+def test_is_not_equal():
+    assert 1 != 2, "number is equal"

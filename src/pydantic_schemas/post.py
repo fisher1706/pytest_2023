b25bbd1be3d9413b07d837ed4563015a09ef34e9
Oleh Lazeba <oleh.lazeba@cd.p2h.com>
2024-02-22 13:55:56 +0200

    some update

--- src/pydantic_schemas/post.py
+++ src/pydantic_schemas/post.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel, Field
 
+"""
+all class inherited from BaseModel
+"""
+
 
 class Post(BaseModel):
     id: int = Field(le=4)
     title: str
+
+
+data_of_validation = {
+    'id': 1,
+    'title': 'Post 1'
+}
--- tests/something_test.py
+++ tests/something_test.py
@@ -7,6 +7,7 @@ from src.pydantic_schemas.post import Post
 
 def test_getting_posts():
     r = requests.get(url=SERVICE_URL)
+    print(f"\nr_json: {r.json()}")
     response = Response(r)
 
     response.assert_status_code(200).validate(Post)
